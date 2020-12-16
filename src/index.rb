20d912848e1e1bd9ac7341adc8185ebf9b5ef9d0
teraglin <apkirvan@hotmail.com>
2020-12-16 15:35:04 +1100

    added menu in index.rb

--- src/index.rb
+++ src/index.rb
@@ -1,5 +1,14 @@
-prompt.warn("
-                                                                        
+require "tty-prompt"
+prompt = TTY::Prompt.new(active_color: :magenta, symbols: {marker: "⬢"})
+require_relative 'dice.rb'
+require_relative 'player.rb'
+require_relative 'enemy.rb'
+require_relative 'combat.rb'
+
+system('clear')
+
+prompt.warn(
+    "                                                                    
     @@@@@@@@@@    @@@@@@   @@@  @@@   @@@@@@   @@@@@@@  @@@@@@@@  @@@@@@@   
     @@@@@@@@@@@  @@@@@@@@  @@@@ @@@  @@@@@@@   @@@@@@@  @@@@@@@@  @@@@@@@@  
     @@! @@! @@!  @@!  @@@  @@!@!@@@  !@@         @@!    @@!       @@!  @@@  
@@ -21,6 +30,29 @@ prompt.warn("
     !!:  !!!  !!: :!!   !!:  !!!  !!:  !!:  !!:  !!:                        
     :!:  !:!  :!:  !:!  :!:  !:!  :!:  :!:  :!:   :!:                       
      :: ::::  ::   :::  ::   :::   :::: :: :::    :: ::::                   
-    :: : ::    :   : :   :   : :    :: :  : :    : :: : :                   
-                                                                            
-    ")
+    :: : ::    :   : :   :   : :    :: :  : :    : :: : :                                                                           
+"
+)
+
+input = prompt.select(' ') do |menu|
+    menu.choice "START"
+    menu.choice "HOW TO PLAY"
+    menu.choice "DICE"
+    menu.choice "QUIT"
+end
+
+if input == "START"
+    load('arena.rb')
+elsif input == "HOW TO PLAY"
+    exit
+elsif input == "DICE"
+    load('dice_roll.rb')
+elsif input == "QUIT"
+    system('clear')
+    puts "See you later!"
+    exit
+end
+
+
+
+    
